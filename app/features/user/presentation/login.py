bedb72f16e161ed41f43d95c508d7ee453ec0268
gabrielguglik <gabrielguglielmik@gmail.com>
2023-04-10 08:33:41 -0300

    adicionado bt recuperar senha

--- app/features/user/presentation/login.py
+++ app/features/user/presentation/login.py
@@ -32,16 +32,19 @@ class Login:
         self.entrada_password = Entry(self.frame_2, bg='#f4bc44', fg='white')
         self.entrada_password.place(relx=0, rely=0.32, relwidth=0.7, relheight=0.06)
 
-        self.lb_recuperar_senha = Label(self.frame_2, text="Recuperar senha", font=('JasmineUPC', 10), bg='#fff',
-                              fg='#f4bc44')
-        self.lb_recuperar_senha.place(relx=0, rely=0.40, relwidth=0.7, relheight=0.06)
+        # self.lb_recuperar_senha = Label(self.frame_2, text="Recuperar senha", font=('JasmineUPC', 10), bg='#fff',
+        #                       fg='#f4bc44')
+        # self.lb_recuperar_senha.place(relx=0, rely=0.40, relwidth=0.7, relheight=0.06)
 
 
     def botoes(self):
-        self.bt_1 = Button(self.frame_1, text="ENTRAR", fg='white', bg='#f4bc44', font=('JasmineUPC', 8))
-        self.bt_1.place(relx=0.25, rely=0.6, relwidth=0.7, relheight=0.15)
+        self.bt_entrar = Button(self.frame_1, text="ENTRAR", fg='white', bg='#f4bc44', font=('JasmineUPC', 8))
+        self.bt_entrar.place(relx=0.25, rely=0.6, relwidth=0.7, relheight=0.15)
 
-        self.bt_2 = Button(self.frame_2, text="VOLTAR", fg='white', bg='#f4bc44', font=('JasmineUPC', 8))
-        self.bt_2.place(relx=0, rely=0.6, relwidth=0.7, relheight=0.15)
+        self.bt_sair = Button(self.frame_2, text="VOLTAR", fg='white', bg='#f4bc44', font=('JasmineUPC', 8))
+        self.bt_sair.place(relx=0, rely=0.6, relwidth=0.7, relheight=0.15)
+
+        self.bt_recuperar_senha = Button(self.frame_2, text="Recuperar senha", fg='#f4bc44', bg='white', font=('JasmineUPC', 10))
+        self.bt_recuperar_senha.place(relx=0.09, rely=0.4, relwidth=0.5, relheight=0.06)
 
 Login()
