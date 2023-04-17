e2b9bb19ab0ee8eaa70b1127a04ab7a347c10adb
Kamilly Victória Ruseler <KAMIRUSELER@GMAIL.COM>
2023-04-16 22:57:26 -0300

    Fazendo classes roomie_detail_page e roomie_detail_ui, seguindo o padrão da classe room. Também adicionado método str_to_bool

--- app/features/roomie/presentations/ad_roomie.py
+++ app/features/roomie/presentations/ad_roomie.py
@@ -115,31 +115,34 @@ class AdRoomie:
             self.bt_1.config(image=imagem_tk)
             self.bt_1.image = imagem_tk
 
+    def str_to_bool(self, value):
+        if value == 'Sim':
+            return True
+        elif value == 'Não':
+            return False
+
     def save_ad(self):
         price = self.current_value.get()
         living_type = self.living_type.get()  # tipo de quarto
-        print(living_type)
         roommate_type = self.roommate_type.get()  # tipo de convivência
         about = self.about.get()
-        is_student = self.is_student.get()
-        is_smoker = self.is_smoker.get()
-        has_pets = self.has_pets.get()
-        has_children = self.has_children.get()
-        has_job = self.has_job.get()
+        is_student = self.str_to_bool(self.is_student.get())
+        is_smoker = self.str_to_bool(self.is_smoker.get())
+        has_pets = self.str_to_bool(self.has_pets.get())
+        has_children = self.str_to_bool(self.has_children.get())
+        has_job = self.str_to_bool(self.has_job.get())
 
         print(
             "Saving roomie: About={}, Is Student={}, Is Smoker={}, Has Pets={}, Has Children={}, Has Job={}, "
             "Price={}, Living Type={}, Roommate Type={}".format(
                 about, is_student, is_smoker, has_pets, has_children, has_job, price, living_type, roommate_type))
 
-        # saída obtida: Saving roomie: About=a, Is Student=Não, Is Smoker=Não, Has Pets=Não
-        # Has Children=Não, Has Job=Não, Price=2888.0, Living Type=Compartilhado, Roommate Type=Regular
-
         roomie = Roomie(price=price, share_date=123, active=True, roommate_type=roommate_type,
                         living_type=living_type, about=about, is_student=is_student, is_smoker=is_smoker,
                         has_pets=has_pets, has_children=has_children, has_job=has_job)
 
         print(roomie)
+
         # saída obtida: <app.features.roomie.entities.roomie.Roomie object at 0x000002316DF5C4C0>
 
 
---
+++ app/features/roomie/presentations/roomie_detail_page.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+from app.commons.colors.colors import *
+from app.commons.navigation import Navigation
+from app.features.roomie.entities import roomie
+from app.features.roomie.presentations.roomie_detail_ui import RoomieDetailUI
+
+
+class RoomieDetailPage:
+    def __init__(self, roomie=None):
+        self.raiz = Tk()
+        self.navigation = None
+        self.ui = RoomieDetailUI(self.raiz, roomie=roomie)
+
+        self.delete_button = Button(self.ui.base.bottom_frame, text="EXCLUIR ANÚNCIO", bg=kYellow, fg=kWhite, command=self.delete)
+        self.delete_button.place(relx=0, rely=0, relheight=1, relwidth=0.3)
+
+        self.edit_button = Button(self.ui.base.bottom_frame, text="EDITAR ANÚNCIO", bg=kYellow, fg=kWhite, command=self.edit)
+        self.edit_button.place(relx=0.35, rely=0, relheight=1, relwidth=0.3)
+
+        self.back_button = Button(self.ui.base.bottom_frame, text="VOLTAR", bg=kYellow, fg=kWhite, command=self.back)
+        self.back_button.place(relx=0.7, rely=0, relheight=1, relwidth=0.3)
+
+        self.raiz.mainloop()
+
+    def back(self):
+        self.navigation = Navigation.BACK
+        self.raiz.destroy()
+
+    def delete(self):
+        self.navigation = Navigation.DELETE
+        self.raiz.destroy()
+
+    def edit(self):
+        self.navigation = Navigation.PUT
+        self.raiz.destroy()
+
+#RoomDetailPage(room=RoomAd(email='mail', district='distrito', images=["C:/Users/victo/Pictures/edited/IMG_1529.jpg", "C:/Users/victo/Downloads/20230315_190422.jpg",  "C:/Users/victo/Downloads/20230315_190422.jpg"], children=True, advance=False))
+
+
+
