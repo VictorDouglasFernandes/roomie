6e4606c804ba2280bb6870bd27c02ee7a4c3fa91
Kamilly Victória Ruseler <KAMIRUSELER@GMAIL.COM>
2023-06-24 22:01:05 -0300

    Feito função de avaliar quarto. Verificando bug em showmessage

--- app/features/room/presentation/room_ad_page.py
+++ app/features/room/presentation/room_ad_page.py
@@ -6,29 +6,38 @@ from app.features.room.presentation.room_detail_ui import RoomDetailUI
 
 
 class RoomAdPage:
+
     def __init__(self, room=None, creator=None):
         self.raiz = Tk()
         self.navigation = None
         self.ui = RoomDetailUI(self.raiz, room=room)
 
         self.back_button = Button(self.ui.base.bottom_frame, text="VOLTAR", bg=kYellow, fg=kWhite, command=self.back)
-        self.back_button.place(relx=0.72, rely=0, relheight=1, relwidth=0.28)
+        self.back_button.place(relx=0.79, rely=0, relheight=1, relwidth=0.21)
+
+        self.rating = Button(self.ui.base.bottom_frame, text="AVALIAÇÕES", bg=kYellow, fg=kWhite,
+                             command=self.rating)
+        self.rating.place(relx=0.26, rely=0, relheight=1, relwidth=0.21)
 
         if not creator:
-            self.qea_button = Button(self.ui.base.bottom_frame, text="PERGUNTAS E RESPOSTAS", bg=kYellow, fg=kWhite,
-                                 command=self.questions)
-            self.qea_button.place(relx=0, rely=0, relheight=1, relwidth=0.28)
+            self.qea_button = Button(self.ui.base.bottom_frame, text="PERGUNTAS\nE RESPOSTAS", bg=kYellow, fg=kWhite,
+                                     command=self.questions)
+            self.qea_button.place(relx=0, rely=0, relheight=1, relwidth=0.21)
 
             self.interest_button = Button(self.ui.base.bottom_frame, text="TENHO\nINTERESSE", bg=kYellow, fg=kWhite,
-                                      command=self.interested)
-            self.interest_button.place(relx=0.34, rely=0, relheight=1, relwidth=0.28)
+                                          command=self.interested)
+            self.interest_button.place(relx=0.52, rely=0, relheight=1, relwidth=0.21)
 
         self.raiz.mainloop()
 
     def questions(self):
         self.navigation = Navigation.QUESTIONS
         self.raiz.destroy()
-    
+
+    def rating(self):
+        self.navigation = Navigation.RATING
+        self.raiz.destroy()
+
     def interested(self):
         self.navigation = Navigation.INTEREST
         self.raiz.destroy()
@@ -36,3 +45,5 @@ class RoomAdPage:
     def back(self):
         self.navigation = Navigation.BACK
         self.raiz.destroy()
+
+
